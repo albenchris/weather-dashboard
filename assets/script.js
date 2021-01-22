9d56f1fb2ce9c6a3221bb745fc9088a6e0a20cd2
albenchris <albenchris00@gmail.com>
2021-01-21 18:35:35 -0600

    initial html scaffolding

---
+++ assets/script.js
@@ -0,0 +1,17 @@
+
+
+
+
+
+
+// CHECKLIST:
+// build basic html elements
+    // (header searchbar, search history, results container, 5-day forecast container)
+// style using grid system
+
+// city typed into input is added to search history section
+// search function to gather weather information for city selected
+    // (city name, date, icon representing weather conditions, temp, humidity, wind speed, UV index)
+// style UV index with color based on favorable, moderate or severe conditions
+// 5-day forecast displays date, weather icon, temp, humidity
+// cities in search history act as buttons to gather information
