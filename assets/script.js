425400c0a2d2865bfbb8588fbadd102c72c8d32e
albenchris <albenchris00@gmail.com>
2021-01-22 16:39:49 -0600

    search bar input returns data

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,4 @@
-
-
-
-
-
-
 // CHECKLIST:
-// build basic html elements
-    // (header searchbar, search history, results container, 5-day forecast container)
 // style using grid system
 
 // city typed into input is added to search history section
@@ -15,3 +7,25 @@
 // style UV index with color based on favorable, moderate or severe conditions
 // 5-day forecast displays date, weather icon, temp, humidity
 // cities in search history act as buttons to gather information
+
+var searchButtonEl = document.getElementById("search-btn");
+
+function findCity(event) {
+    event.preventDefault();
+    var userInput = document.getElementById("user-input").value;
+    var OpenWeatherUrl = "http://api.openweathermap.org/data/2.5/find?q=" + 
+        userInput + 
+        "&units=imperial&appid=90a1a6aec56ae63b28d2c0abbe206092"; // api key
+
+    fetch(OpenWeatherUrl).then(function(response) {
+        return response.json();
+    }).then(function(response) {
+        // console.log(response.list[0].main.temp_max);
+        // console.log(response.list[0].main.temp_min);
+        // console.log(response.list[0].main.feels_like);
+        console.log(response);
+    });
+    
+};
+
+searchButtonEl.addEventListener("click", findCity);
