7ec27dafe059c7b5a4c59174edca9ffc49f5ab31
albenchris <albenchris00@gmail.com>
2021-01-23 16:41:42 -0600

    displayFiveDay initial functionality

--- assets/script.js
+++ assets/script.js
@@ -6,10 +6,14 @@
 // 5-day forecast displays date, weather icon, temp, humidity
 // cities in search history act as buttons to gather information
 
+var currentDate = new Date();
+
 var searchFormEl = document.getElementById("search-form");
 var userInput = document.getElementById("city-name");
 var currentWeatherEl = document.getElementById("current-weather-container");
 var fiveDayEl = document.getElementById("five-day-container");
+var eachDayEl = document.getElementById("each-day-container");
+
 
 function getWeather(city) {
     var OpenWeatherUrl = "http://api.openweathermap.org/data/2.5/forecast?q=" + 
@@ -20,7 +24,7 @@ function getWeather(city) {
         return response.json();
     }).then(function(response) {
         // console.log(response); // use to display city name on page
-        document.getElementById("city-header").textContent = response.city.name;
+        document.getElementById("city-header").textContent = response.city.name + " " + currentDate;
 
         if (response) {
             return fetch("https://api.openweathermap.org/data/2.5/onecall?" + 
@@ -31,14 +35,13 @@ function getWeather(city) {
         return secondResponse.json();
     }).then(function(weather) {
         displayCurrent(weather);
-    });
-    
+        displayFiveDay(weather);
+    }); 
 };
 
 function displayCurrent(weather) {
-    currentWeatherEl.classList.remove("hide")
+    currentWeatherEl.classList.remove("hide");
 
-    document.getElementById("date-span").textContent = new Date();
     document.getElementById("current-temp").textContent = weather.current.temp + "°F";
     document.getElementById("feels-like").textContent = weather.current.feels_like + "°F";
     document.getElementById("current-humidity").innerHTML = weather.current.humidity + "%";
@@ -47,12 +50,47 @@ function displayCurrent(weather) {
     document.getElementById("wind-chill").innerHTML = weather.current.wind_deg + "°F";
 };
 
-function displayFiveDay() {
+function displayFiveDay(weather) {
+    console.log(weather);
+    fiveDayEl.classList.remove("hide");
+    eachDayEl.innerHTML = "";
 
+    // date, weather icon
+    for (var d=0; d<5; d++) {
+        var eachDateEl = document.createElement("h4");
+
+        Date.prototype.addDays = function(days) {
+            var date = new Date(this.valueOf());
+            date.setDate(date.getDate() + days);
+            return date;
+        }
+        eachDateEl.textContent = currentDate.addDays(d);
+        var dailyWeatherIcon = document.createElement("span"); // place inside eachDateEl
+        dailyWeatherIcon.textContent = "";
+        eachDateEl.appendChild(dailyWeatherIcon);
+        
+        fiveDayEl.appendChild(eachDateEl);
+
+        var dailyHighEl = document.createElement("p");
+        dailyHighEl.textContent = "High: " + weather.daily[d].temp.max + "°F";
+
+        var dailyLowEl = document.createElement("p");
+        dailyLowEl.textContent = "Low: " + weather.daily[d].temp.min + "°F";
+
+        var dailyHumidityEl = document.createElement("p");
+        dailyHumidityEl.textContent = "Humidity: " + weather.daily[d].humidity + "%";
+
+        fiveDayEl.appendChild(dailyHighEl);
+        fiveDayEl.appendChild(dailyLowEl);
+        fiveDayEl.appendChild(dailyHumidityEl);
+    }
     
 };
 
+
+// displayCurrent();
 // displayFiveDay();
+
 
 
 
@@ -68,9 +106,6 @@ function formSubmitHandler(event) {
     } else {
         console.log("something went wrong");
     }
-
-    
-
 
 };
 
