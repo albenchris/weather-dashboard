37be2ea0b3103c6311fe5f8688881cddf12b41bd
albenchris <albenchris00@gmail.com>
2021-01-24 17:11:09 -0600

    date displayed changes based on city's time zone

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,12 @@
-// CHECKLIST:
-// style using grid system
+// global variables start
+// var currentDate = new Date()
+// localTime = currentDate.getTime()
+// localOffset = currentDate.getTimezoneOffset() * 60000
+// utc = localTime + localOffset
+// var thisCity = utc + (1000 * -weatherData.timezone_offset)
+// newDate = new Date(thisCity);
 
-// style UV index with color based on favorable, moderate or severe conditions
-
-var currentDate = new Date();
+// console.log(moment.tz.names());
 
 var searchFormEl = document.getElementById("search-form");
 var userInput = document.getElementById("city-name");
@@ -13,8 +16,9 @@ var cityNamesArr = JSON.parse(localStorage.getItem("cityNamesArr")) || [];
 var currentWeatherEl = document.getElementById("current-weather-container");
 var fiveDayEl = document.getElementById("five-day-container");
 var eachDayEl = document.getElementById("each-day-container");
+// global variables end
 
-
+// function definitions start
 function getWeather(city) {
     var OpenWeatherUrl = "http://api.openweathermap.org/data/2.5/forecast?q=" + 
         city + "&units=imperial&appid=90a1a6aec56ae63b28d2c0abbe206092";
@@ -22,10 +26,13 @@ function getWeather(city) {
     fetch(OpenWeatherUrl).then(function(response) {
         return response.json();
     }).then(function(response) {
-        console.log(response);
+        // console.log(response);
         var iconIMG = "<img src=http://openweathermap.org/img/wn/" + response.list[0].weather[0].icon + "@2x.png />";
 
-        document.getElementById("city-header").innerHTML = response.city.name + " " + currentDate + iconIMG;
+        document.getElementById("city-header").innerHTML = 
+            response.city.name + ", " +
+            response.city.country +
+            iconIMG;
         saveCityName(response);
 
         if (response) {
@@ -36,6 +43,7 @@ function getWeather(city) {
     }).then(function(secondResponse) {
         return secondResponse.json();
     }).then(function(weatherData) {
+        changeTimeAndDate(weatherData);
         displayCurrent(weatherData);
         displayFiveDay(weatherData);
     }); 
@@ -43,6 +51,8 @@ function getWeather(city) {
 
 function displayCurrent(weatherData) {
     currentWeatherEl.classList.remove("hide");
+
+    document.getElementById("current-date").textContent = thisCityDate;
 
     document.getElementById("current-temp").textContent = weatherData.current.temp + "°F";
     document.getElementById("feels-like").textContent = weatherData.current.feels_like + "°F";
@@ -64,7 +74,7 @@ function displayCurrent(weatherData) {
 };
 
 function displayFiveDay(weatherData) {
-    console.log(weatherData);
+    // console.log(weatherData);
     fiveDayEl.classList.remove("hide");
     eachDayEl.innerHTML = "";
 
@@ -72,12 +82,13 @@ function displayFiveDay(weatherData) {
     for (var d=0; d<5; d++) {
         var eachDateEl = document.createElement("h4");
 
-        Date.prototype.addDays = function(days) {
-            var date = new Date(this.valueOf());
-            date.setDate(date.getDate() + days);
-            return date;
-        }
-        eachDateEl.textContent = currentDate.addDays(d);
+        function addDays(date, days) {
+            var copy = new Date(Number(date));
+            copy.setDate(date.getDate() + days);
+            return copy;
+        };
+
+        eachDateEl.textContent = addDays(thisCityDate, d); /*moment().tz(weatherData.timezone).add(d, "days").format("ddd MMM Do")*/
         var dailyWeatherIcon = document.createElement("img");
         dailyWeatherIcon.setAttribute("src", "http://openweathermap.org/img/wn/" + weatherData.daily[d].weather[0].icon + "@2x.png");
         eachDateEl.appendChild(dailyWeatherIcon);
@@ -99,10 +110,23 @@ function displayFiveDay(weatherData) {
     }
 };
 
+function changeTimeAndDate(weatherData) {
+    var currentDate = new Date();
+    console.log(currentDate);
+    localTime = currentDate.getTime();
+    localOffset = currentDate.getTimezoneOffset() * 60000;
+    utc = localTime + localOffset;
+    var thisCityTime = utc + (1000 * weatherData.timezone_offset);
+    thisCityDate = new Date(thisCityTime);
+
+    return thisCityDate;
+};
+
 function saveCityName(response) {
     var cityName = response.city.name;
     cityNamesArr.push(cityName);
     cityNamesArr.reverse((a,b) => a - b);
+    cityNamesArr.splice(6);
 
     localStorage.setItem("cityNamesArr", JSON.stringify(cityNamesArr));
 
@@ -114,7 +138,6 @@ function createSearchButton() {
 
     for (var i=0; i<cityNamesArr.length; i++) {
         var cityNameButton = document.createElement("button");
-        // cityNameButton.setAttribute("id", "city-button-" + i);
         cityNameButton.setAttribute("city-name", cityNamesArr[i]);
         cityNameButton.textContent = cityNamesArr[i];
 
@@ -142,8 +165,10 @@ function buttonClickHandler(event) {
         getWeather(cityName);
     }
 };
+// function definitions end
 
 createSearchButton();
 
+// event listeners
 searchFormEl.addEventListener("submit", formSubmitHandler);
 searchHistoryEl.addEventListener("click", buttonClickHandler);
